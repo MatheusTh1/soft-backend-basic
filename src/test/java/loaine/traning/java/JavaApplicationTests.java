8ecbb8cc3e3e883a53a012c8bf2c982c177fc1e3
Matheus <matheushenriquesilva.th@gmail.com>
2022-07-14 23:54:20 -0300

    estrutura do projeto

---
+++ src/test/java/loaine/traning/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package loaine.traning.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
